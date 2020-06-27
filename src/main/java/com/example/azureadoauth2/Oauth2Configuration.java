cb7f6f9d89efc39b436c5d7be61cce4ab90622cc
csuttic <chan.suttichujit@exxonmobil.com>
2020-06-27 22:51:28 +0700

    add oauth2 client as an authentication method

--- src/main/java/com/example/azureadoauth2/Oauth2Configuration.java
+++ src/main/java/com/example/azureadoauth2/Oauth2Configuration.java
@@ -9,11 +9,10 @@ import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 @Configuration
 public class Oauth2Configuration extends WebSecurityConfigurerAdapter {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.
-                authorizeRequests(a -> a.antMatchers("/azuread")
-                        .fullyAuthenticated())
-                .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)));
-    }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests(a -> a.antMatchers("/azuread").fullyAuthenticated())
+        .exceptionHandling(e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+        .oauth2Client();
+  }
 }
