d01914f5ab696bae4bf3650ce463008e86e56064
csuttic <chan.suttichujit@exxonmobil.com>
2020-07-27 17:09:12 +0700

    change hasRole to hasAuthority

--- src/main/java/com/example/azureadoauth2/HelloController.java
+++ src/main/java/com/example/azureadoauth2/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/azuread")
-    @PreAuthorize("hasRole('Reader')")
+    @PreAuthorize("hasAuthority('Reader')")
     public String azureAD() {
         return "Hello AzureAD";
     }
